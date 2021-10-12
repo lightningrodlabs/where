8e0c5a8ee88bb48b4b9411e744e7333164b735f0
Eric Harris-Braun <eric@harris-braun.com>
2021-10-12 11:07:39 -0400

    Update to new way of doing full

--- ui/apps/where-we/src/index.ts
+++ ui/apps/where-we/src/index.ts
@@ -2,18 +2,16 @@ import { AppWebsocket, InstalledCell } from "@holochain/conductor-api";
 import { html, render } from "lit";
 
 import { WhereApp } from "./where-app";
+import { ScopedElementsHost } from "@open-wc/scoped-elements/types/src/types";
 
 export default function (appWebsocket: AppWebsocket, cellData: InstalledCell) {
   return {
-    full(element: HTMLElement, registry: CustomElementRegistry) {
-      registry.define("where-app", WhereApp);
-      render(
-        html`<where-app
-          .appWebsocket=${appWebsocket}
-          .cellData=${cellData}
-        ></where-app>`,
-        element
-      );
+    full(element: HTMLElement, host: ScopedElementsHost) {
+      host.defineScopedElement("where-app", WhereApp);
+      element.innerHTML = `<where-app></where-app>`;
+      let app = (element.querySelector('where-app') as any)
+      app.appWebsocket = appWebsocket
+      app.cellData = cellData
     },
     blocks: [],
   };

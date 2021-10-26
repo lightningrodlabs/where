4c6f4c69d1f385d98ec280e6b421c289073085c1
Eric Harris-Braun <eric@harris-braun.com>
2021-10-26 17:47:35 -0400

    Fixed breaking DNA test

--- tests/src/where.ts
+++ tests/src/where.ts
@@ -16,18 +16,6 @@ export default async (orchestrator) => {
     // be used to spin up the conductor processes which are returned in a matching array.
     const [a_and_b_conductor] = await s.players([localConductorConfig])
 
-    a_and_b_conductor.setSignalHandler((signal) => {
-      console.log("Received Signal:",signal)
-      t.deepEqual(signal.data.payload.message, { type: 'NewSpace', content: { name: 'mountain map', surface: 'https://mountain-map-images.com/cotopaxi', meta: {} } })
-    })
-
-    // install your happs into the conductors and destructuring the returned happ data using the same
-    // array structure as you created in your installation array.
-    let [alice_where_happ/*, bobbo_where_happ*/] = await installAgents(a_and_b_conductor, ["alice"/*, 'bobbo'*/])
-    const [alice_where] = alice_where_happ.cells
-//    const [bobbo_where] = bobbo_where_happ.cells
-
-    // Create template
     let template1 = {
       name: "Map",
       surface: "{\
@@ -37,19 +25,9 @@ export default async (orchestrator) => {
       }",
     };
 
-    const template1_eh64 = await alice_where.call('hc_zome_where', 'create_template', template1 );
-    t.ok(template1_eh64)
-    console.log("template1_eh64", template1_eh64);
-
-    const templates = await alice_where.call('hc_zome_where', 'get_templates', null );
-    console.log(templates);
-    t.deepEqual(templates, [{hash: template1_eh64, content: template1}]);
-
-
-    // Create a space
     let space1 = {
       name: "mountain map",
-      origin: template1_eh64,
+      origin: "placeholder",
 /*      dimensionality: {
         type: "orthogonal",
         coords: {x: "integer", y:"integer"},
@@ -61,6 +39,35 @@ export default async (orchestrator) => {
       }",
       meta: {}
     };
+
+    a_and_b_conductor.setSignalHandler((signal) => {
+      console.log("Received Signal:",signal)
+      if (signal.data.payload.message.type == 'NewSpace') {
+        t.deepEqual(signal.data.payload.message.content, space1)
+      } else {
+        t.deepEqual(signal.data.payload.message.content, template1)        
+      }
+    })
+
+    // install your happs into the conductors and destructuring the returned happ data using the same
+    // array structure as you created in your installation array.
+    let [alice_where_happ/*, bobbo_where_happ*/] = await installAgents(a_and_b_conductor, ["alice"/*, 'bobbo'*/])
+    const [alice_where] = alice_where_happ.cells
+//    const [bobbo_where] = bobbo_where_happ.cells
+
+    // Create template
+ 
+    const template1_eh64 = await alice_where.call('hc_zome_where', 'create_template', template1 );
+    t.ok(template1_eh64)
+    console.log("template1_eh64", template1_eh64);
+
+    const templates = await alice_where.call('hc_zome_where', 'get_templates', null );
+    console.log(templates);
+    t.deepEqual(templates, [{hash: template1_eh64, content: template1}]);
+
+
+    // Create a space
+    space1.origin = template1_eh64
 
     const space1_hash = await alice_where.call('hc_zome_where', 'create_space', space1 );
     t.ok(space1_hash)

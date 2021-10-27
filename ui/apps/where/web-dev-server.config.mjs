972a00de1ab302f7c39c8f7f2703b09372110269
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-10-27 11:01:29 +0200

    Improved toolchain

--- ui/apps/where/web-dev-server.config.mjs
+++ ui/apps/where/web-dev-server.config.mjs
@@ -1,27 +1,45 @@
 // import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
+import rollupReplace from '@rollup/plugin-replace';
+import rollupCommonjs from '@rollup/plugin-commonjs';
+import { fromRollup } from '@web/dev-server-rollup';
+import rollupBuiltins from 'rollup-plugin-node-builtins';
+
+const replace = fromRollup(rollupReplace);
+const commonjs = fromRollup(rollupCommonjs);
+const builtins = fromRollup(rollupBuiltins);
 
 /** Use Hot Module replacement by adding --hmr to the start command */
-const hmr = process.argv.includes("--hmr");
-
-const HC_PORT = process.env.HC_PORT || 8888;
-const DIST_FOLDER = `.dist/${HC_PORT}`;
+const hmr = process.argv.includes('--hmr');
 
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
-  rootDir: DIST_FOLDER,
-  open: "/",
+  open: true,
   watch: !hmr,
   /** Resolve bare module imports */
   nodeResolve: {
-    exportConditions: ["browser", "development"],
+    preferBuiltins: false,
+    browser: true,
+    exportConditions: ['browser', 'development'],
   },
 
   /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
   // esbuildTarget: 'auto'
 
+  rootDir: '../../../',
+
   /** Set appIndex to enable SPA routing */
-  // appIndex: 'demo/index.html',
+  appIndex: './index.html',
 
   plugins: [
+    replace({
+      'process.env.NODE_ENV': `"production"`,
+      'process.env.ENV': JSON.stringify(process.env.ENV),
+      'process.env.HC_PORT': JSON.stringify(process.env.HC_PORT || 8888),
+      '  COMB =': 'window.COMB =',
+      delimiters: ['', ''],
+    }),
+    builtins(),
+    commonjs({}),
+
     /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
     // hmr && hmrPlugin({ exclude: ['**/*/node_modules/**/*'], presets: [presets.litElement] }),
   ],

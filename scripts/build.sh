a8e1cb46dfee6d0c2617f53201748d6fd6cd89f5
damien <doute.d@gmail.com>
2022-04-09 16:47:45 -0400

    Fix script for hash zome

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,10 +6,12 @@
 # where it calls install-hc-tools and and dna-pack
 
 # ensure all necessary binaries are packaged in the app
-rm -rf electron/binaries
-mkdir electron/binaries
+#rm -rf electron/binaries
+#mkdir electron/binaries
 cp dna/workdir/dna-ludotheque/where.dna electron/binaries/where.dna
 cp dna/workdir/happ-where/where.happ electron/binaries/where.happ
+cp dna/where_zome_hash.txt electron/binaries
+
 bash scripts/copy-binaries.sh
 
 # ui

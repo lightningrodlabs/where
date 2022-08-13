1217cd76c6014844e2724c5e339dad4d3b921ca2
damien <doute.d@gmail.com>
2022-08-12 21:04:17 -0400

    Broken rustc workaround

--- scripts/install-hc-tools.sh
+++ scripts/install-hc-tools.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+rustc --version
+rustup install 1.61.0
+rustup override set 1.61.0
+
 # install wasm32 compilation target
 rustup target install wasm32-unknown-unknown
 
 # install `hc` cli tool
 # KEEP THIS IN SYNC
-cargo install holochain_cli --version 0.0.28
+cargo install holochain_cli --version 0.0.41

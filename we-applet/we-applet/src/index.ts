181ac1f770f2a076610356d5487b71467ebcd8aa
damien <doute.d@gmail.com>
2022-09-14 11:26:35 -0400

    Initial import of we-applet

---
+++ we-applet/we-applet/src/index.ts
@@ -0,0 +1,38 @@
+import {
+  AdminWebsocket,
+  AppWebsocket,
+  InstalledAppInfo,
+  InstalledCell,
+} from "@holochain/client";
+import {
+  WeApplet,
+  AppletRenderers,
+  WeServices,
+  InstalledAppletInfo,
+} from "@lightningrodlabs/we-applet";
+
+import { WhereApplet } from "./where-applet";
+
+const whereApplet: WeApplet = {
+  async appletRenderers(
+    appWebsocket: AppWebsocket,
+    adminWebsocket: AdminWebsocket,
+    weServices: WeServices,
+    appletAppInfo: InstalledAppletInfo[]
+  ): Promise<AppletRenderers> {
+    return {
+      full(element: HTMLElement, registry: CustomElementRegistry) {
+        registry.define("where-applet", WhereApplet);
+        element.innerHTML = `<where-applet></where-applet>`;
+        const appletElement = element.querySelector("where-applet") as any;
+
+        appletElement.appWebsocket =  appWebsocket;
+        appletElement.profilesStore = weServices.profilesStore;
+        appletElement.appletAppInfo = appletAppInfo;
+      },
+      blocks: [],
+    };
+  },
+};
+
+export default whereApplet;

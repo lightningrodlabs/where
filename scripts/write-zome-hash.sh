ff03f513f293e65e214887c5b7136cae17f0ede6
damien <doute.d@gmail.com>
2022-10-20 09:45:45 -0400

    Streamlined artifact names and scripts

--- scripts/write-zome-hash.sh
+++ scripts/write-zome-hash.sh
@@ -1,11 +1,11 @@
 # Compute hash of where zome
-value=`./hash_zome ./target/wasm32-unknown-unknown/release/where.wasm`
+value=`./hash_zome ./target/wasm32-unknown-unknown/release/where_zome.wasm`
 echo "$value" > electron/bin/where_zome_hash.txt
 echo
 echo "     WHERE ZOME HASH = $value"
 
 # Compute hash of ludotheque zome
-value=`./hash_zome ./target/wasm32-unknown-unknown/release/where_ludotheque.wasm`
+value=`./hash_zome ./target/wasm32-unknown-unknown/release/where_ludotheque_zome.wasm`
 echo "$value" > electron/bin/ludotheque_zome_hash.txt
 echo
 echo "LUDOTHEQUE ZOME HASH = $value"

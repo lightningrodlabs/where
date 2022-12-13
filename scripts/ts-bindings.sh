ef4b09ccca61e2e9c60066ac342da12c018c776e
damien <doute.d@gmail.com>
2022-12-13 15:07:51 -0500

    Fixed ts-bindings script

--- scripts/ts-bindings.sh
+++ scripts/ts-bindings.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-zits -i dna/zomes/ludotheque -i dna/zomes/ludotheque_integrity -o webcomponents/src/bindings/ludotheque.ts
-zits -i dna/zomes/playset -i dna/zomes/playset_integrity -o webcomponents/src/bindings/playset.ts
-zits -i dna/zomes/where -i dna/zomes/where_integrity -o webcomponents/src/bindings/where.ts
-#zits -i dna/zomes/profiles -i dna/zomes/profiles_integrity -o dna/bindings/profiles.ts
-
-
+zits --default-zome-name zLudotheque -i dna/zomes/ludotheque -i dna/zomes/ludotheque_integrity -o webcomponents/src/bindings/ludotheque.ts
+zits --default-zome-name zPlayset -i dna/zomes/playset -i dna/zomes/playset_integrity -o webcomponents/src/bindings/playset.ts
+zits --default-zome-name zWhere -i dna/zomes/where -i dna/zomes/where_integrity -o webcomponents/src/bindings/where.ts

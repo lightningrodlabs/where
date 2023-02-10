e6791bf83374a343a94f9f6d37b7f11d6e9b5796
damien <doute.d@gmail.com>
2023-02-10 11:56:58 -0500

    Use hc-prebuilt and seperate electron workflow

--- scripts/update-version-number.sh
+++ scripts/update-version-number.sh
@@ -34,16 +34,6 @@ OLD_VER=`awk -F ":" '/"version"/ {print $2}' ./webapp.ludotheque/package.json | 
 echo "./webapp.ludotheque/package.json $OLD_VER -> $1"
 sed -i "s/\"version\": \"$OLD_VER\"/\"version\": \"$1\"/" ./webapp/package.json
 
-# Change .github/workflows/release.yml
-OLD_VER=`awk -F ": " '/WHERE_VERSION/{print $2}' ./.github/workflows/release.yml`
-echo "./.github/workflows/release.yml '$OLD_VER' -> '$1'"
-sed -i "s/$OLD_VER/$1/" ./.github/workflows/release.yml
-
-# Change .github/workflows/test.yml
-OLD_VER=`awk -F ": " '/WHERE_VERSION/ {print $2}' ./.github/workflows/test.yml`
-echo "./.github/workflows/test.yml '$OLD_VER' -> '$1'"
-sed -i "s/$OLD_VER/$1/" ./.github/workflows/test.yml
-
 
 # Change electron/web/splashscreen.html
 LINE=`echo version $1`
--- scripts/write-zome-hash.sh
+++ scripts/write-zome-hash.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+set -e
 
 # Script for writing zome hashes to files
 
+echo Executing \"$0\".
+
+# Check pre-conditions
+if [ $# != 1 ]; then
+  echo 1>&2 "$0: Aborting. Missing argument: bin folder path"
+  exit 2
+fi
+
+binFoloder=$1
+
 # Compute hash of where zome
-value=`./bin/hash_zome ./target/wasm32-unknown-unknown/release/where_zome.wasm`
+value=`$binFoloder/hash_zome ./target/wasm32-unknown-unknown/release/where_zome.wasm`
 echo "$value" > electron/bin/where_zome_hash.txt
 echo
 echo "     WHERE ZOME HASH = $value"
 
 # Compute hash of ludotheque zome
-value=`./bin/hash_zome ./target/wasm32-unknown-unknown/release/where_ludotheque_zome.wasm`
+value=`$binFoloder/hash_zome ./target/wasm32-unknown-unknown/release/where_ludotheque_zome.wasm`
 echo "$value" > electron/bin/ludotheque_zome_hash.txt
 echo
 echo "LUDOTHEQUE ZOME HASH = $value"

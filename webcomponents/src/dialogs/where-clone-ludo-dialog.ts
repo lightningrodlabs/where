74634817a98aa13989e25f02b3abc021c7c4e448
damien <doute.d@gmail.com>
2023-03-10 12:24:53 -0500

    set focus

--- webcomponents/src/dialogs/where-clone-ludo-dialog.ts
+++ webcomponents/src/dialogs/where-clone-ludo-dialog.ts
@@ -125,21 +125,34 @@ export class WhereCloneLudoDialog extends ScopedElementsMixin(LitElement) {
         <mwc-button id="primary-action-button" slot="primaryAction" @click=${this.handleCreateSelected}>${msg('Create new')}</mwc-button>
         <mwc-button slot="primaryAction" @click=${this.handleCodeSelected}>${msg('Enter code')}</mwc-button>
       </mwc-dialog>
-      <mwc-dialog id="enter-code-dialog" heading="${msg('Add Library: Enter code')}" @opened=${this.handleDialogOpened}>
+      <mwc-dialog id="enter-code-dialog" heading="${msg('Add Library: Enter code')}" @opened=${() => {
+        this.requestUpdate();
+        const field = this.shadowRoot!.getElementById("uuid-field") as TextField;
+        field.focus();
+      }}>
         <mwc-textfield required helperPersistent outlined style="min-width: 500px;" type="text" id="uuid-field" label="UUID"></mwc-textfield>
         <mwc-button id="primary-action-button" slot="primaryAction" @click=${this.handleJoinLibrary}>${msg('join')}</mwc-button>
         <mwc-button slot="secondaryAction" dialogAction="cancel">${msg('cancel')}</mwc-button>
       </mwc-dialog>
-      <mwc-dialog id="set-name-dialog" heading="${msg('Add Library: Choose name')}" @opened=${this.handleDialogOpened}>
+      <mwc-dialog id="set-name-dialog" heading="${msg('Add Library: Choose name')}" @opened=${() => {
+        this.requestUpdate();
+        const field = this.shadowRoot!.getElementById("set-name-field") as TextField;
+        field.focus();
+      }}>
         <mwc-textfield autoValidate required min="2" helperPersistent helper="${msg("min. 2 characters")}"outlined type="text" id="set-name-field" label="${msg('Name')}"></mwc-textfield>
         <mwc-button id="primary-action-button" raised slot="primaryAction" @click=${this.handleSetNameOk}>${msg('create')}</mwc-button>
       </mwc-dialog>
 
-      <mwc-dialog id="create-new-dialog" heading="${msg('Add Library: Create new')}" @opened=${this.handleDialogOpened}>
+      <mwc-dialog id="create-new-dialog" heading="${msg('Add Library: Create new')}" @opened=${() => {
+          this.requestUpdate();
+          const field = this.shadowRoot!.getElementById("new-clone-name-field") as TextField;
+          field.focus();
+        }}>
         <mwc-textfield autoValidate required min="2" helperPersistent helper="${msg("min. 2 characters")}"outlined type="text" id="new-clone-name-field" label="${msg('Name')}"></mwc-textfield>
         <mwc-button id="primary-action-button" raised slot="primaryAction" @click=${this.handleCreate}>${msg('create')}</mwc-button>
         <mwc-button slot="secondaryAction" dialogAction="cancel">${msg('cancel')}</mwc-button>
       </mwc-dialog>
+
       <mwc-dialog id="show-code-dialog" heading="${msg('Add Library: Create New confirmed')}" @opened=${this.handleDialogOpened}>
         <mwc-textfield outlined .value="${this._uuid}" style="min-width: 500px;" type="text" id="show-uuid-field" label="UUID"></mwc-textfield>
         <mwc-button id="primary-action-button" raised slot="primaryAction" @click=${this.handleOk}>${msg('ok')}</mwc-button>
